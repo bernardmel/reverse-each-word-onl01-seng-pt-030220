2d0c048f43d446e9df46ca4125aaaeb344d29704
Bernard  Mel <bernard.mamuyac@gmail.com>
2020-03-10 02:54:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
+
 def reverse_each_word(sentence)
-  word_reverse = []
-  sentence.each do |reverse|
-  word_reverse << "Hello there, and how are you?"
-end
-word_reverse.reverse!
+  new_sentence = sentence.split(" ")
+  new_sentence.collect {|word| word.reverse!}
+  new_sentence.join(" ")
 end
